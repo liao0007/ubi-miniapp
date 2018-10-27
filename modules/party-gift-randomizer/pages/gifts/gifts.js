7e6a6a221e175e74d43e6b84e4a085fcaca5fffc
Zhou Sha <zhousha@beyondsoft.com>
2018-10-27 17:03:20 +0800

    add gifts page

--- modules/party-gift-randomizer/pages/gifts/gifts.js
+++ modules/party-gift-randomizer/pages/gifts/gifts.js
@@ -7,14 +7,16 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    giftsList: undefined
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    this.setData({
+      giftsList: app.globalData.party.gifts
+    })
   },
 
   /**
